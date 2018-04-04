1484346424b1c944ef0ae928a388f11436cdc83d
tx19980520 <ty0207@sjtu.edu.cn>
2018-04-04 16:47:24 +0800

    get ready for the search page
    为日后进行前端工作做好准备

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import './index.css';
 import "./All.css";
 import store from './Store.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import BilibiliRouter from "./BilibiliRouter.js";
 ReactDOM.render(
     <Provider store={store}>
